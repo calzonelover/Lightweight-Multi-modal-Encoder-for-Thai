a543f0aa1c0fd0f0c8604a4354017bdcee41c34d
calzonelover <patomporn.pay@gmail.com>
2023-05-02 09:28:42 -0700

    bash scripts for multiple model finetuning

--- models/train_lightweights.bash
+++ models/train_lightweights.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python finetune_lightweight_encoder.py --target_save_file projector_only_distill.pt --is_only_distill 1
+python finetune_lightweight_encoder.py --target_save_file projector_only_distill2.pt --is_only_distill 1 --n_epochs 200 --learning_rate 0.1
 
-python finetune_lightweight_encoder.py --target_save_file projector_zero_alpha.pt --alpha 0.0
+python finetune_lightweight_encoder.py --target_save_file projector_high_alpha2.pt --alpha 400.0 --n_epochs 200 --learning_rate 0.1
 
-python finetune_lightweight_encoder.py --target_save_file projector_high_alpha.pt --alpha 200.0
+python finetune_lightweight_encoder.py --target_save_file projector_zero_alpha2.pt --alpha 0.0 --n_epochs 200 --learning_rate 0.1
